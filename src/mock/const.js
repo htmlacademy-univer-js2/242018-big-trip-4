0a5b4ebe0249bd72e6d02c362bf2b027a9e6dae5
luftenmensch <prophetfromgreece@gmail.com>
2024-04-15 16:40:21 +0400

    Отрефакторил код по заданию

--- src/mock/const.js
+++ src/mock/const.js
@@ -5,9 +5,9 @@ export const FULL_TIME_FOMAT = 'YYYY-MM-DDTHH:mm';
 export const SLASH_TIME_FOMAT = 'DD/MM/YY HH:mm';
 export const MILLISECONDS_IN_DAY = 86400000;
 export const MILLISECONDS_IN_HOUR = 3600000;
-export const DESTINATION_COUNT = 4;
-export const POINT_COUNT = 4;
-export const OFFER_COUNT = 7;
+export const POINT_COUNT = 5;
+export const DESTINATION_COUNT = POINT_COUNT;
+export const OFFER_COUNT = 10;
 
 export const BooleanValues = [
   true,
@@ -15,16 +15,24 @@ export const BooleanValues = [
 ];
 
 export const POINT_EMPTY = {
+  id: 1,
   basePrice: 0,
-  dateFrom: null,
-  dateTo: null,
-  destination: null,
+  dateFrom: '',
+  dateTo: '',
+  destination: '',
   ifFavorite: false,
   offers: [],
-  type: 'flight',
+  type: 'Flight',
 };
 
-export const DESCRIPTIONS = [
+export const EMPTY_WARNINGS = {
+  EVERYTHING: 'Click New Event to create your first',
+  FUTURE: 'There are no past events now',
+  PRESENT: 'There are no present events now',
+  PAST: 'There are no future events now'
+};
+
+export const DESCRIPTION = [
   'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.',
   'Aenean commodo ligula eget dolor. Aenean mass.',
   'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
@@ -37,10 +45,10 @@ export const OFFERS = [
   'Close But No Cigar',
   'On the Same Page',
   'Jaws of Death',
-  'Every Cloud Has a Silver Lining',
+  'Every Cloud Has a Silver',
   'Jig Is Up',
   'In a Pickle',
-  'What Goes Up Must Come Down',
+  'What Goes Up Must Come',
   'Break The Ice',
   'In the Red',
 ];
@@ -59,29 +67,26 @@ export const ROUTE_TYPE = [
 
 export const CITIES = [
   'Salisbury',
-  'Kingston upon Hull',
+  'Kingston',
   'Ripon',
   'Liverpool',
   'Carlisle',
   'Oxford',
   'Manchester',
   'Chelmsford',
-  'Carlisle'
 ];
 
-export const DATES = [
-  '2019-07-10T22:55:56.845Z',
-  '2019-07-11T22:55:56.845Z',
-  '2019-07-12T22:55:56.845Z',
-  '2019-07-13T22:55:56.845Z',
-  '2019-07-14T22:55:56.845Z'
-];
+export const FilterType = {
+  EVERYTHING: 'Everything',
+  FUTURE: 'Future',
+  PRESENT: 'Present',
+  PAST: 'Past'
+};
 
-export const id = [
-  'zpJruf3vmWw-8MUyZPWx',
-  'stg9-c9zrK0pu2n99OUe',
-  'YT-SMjFHc7S_uEnU61Th',
-  '-gDk1GeiUafeZJJQ-dpE',
-  'QqbAXKJ_Couc3bhn-ba8',
-  'bcpicZLm9rLOUZ85Y4M_'
-];
+export const SortType = {
+  DAY: 'DAY',
+  EVENT: 'EVENT',
+  TIME: 'TIME',
+  PRICE: 'PRICE',
+  OFFERS: 'OFFERS'
+};
--- src/mock/destination.js
+++ src/mock/destination.js
@@ -1,12 +1,12 @@
 import {getRandomArrayElement, getPicturesArray} from '../lib/util.js';
-import {CITIES, DESCRIPTIONS} from './const.js';
+import {CITIES, DESCRIPTION} from './const.js';
 
 export const generateDestination = () => {
   const city = getRandomArrayElement(CITIES);
 
   return {
     id: crypto.randomUUID(),
-    description: getRandomArrayElement(DESCRIPTIONS),
+    description: getRandomArrayElement(DESCRIPTION),
     name: city,
     pictures: getPicturesArray(city)
   };
