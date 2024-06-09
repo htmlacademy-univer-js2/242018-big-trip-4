9f3176c78b96cf938b3ba0b4b0e23b3a6acaee23
luftenmensch <prophetfromgreece@gmail.com>
2024-06-09 16:45:44 +0400

    Отрефакторил код

--- src/mock/offer.js
+++
@@ -1,9 +0,0 @@
-import {getRandomArrayElement, getRandomInt} from '../lib/util.js';
-import {OFFERS} from '../const.js';
-
-
-export const generateOffer = () => ({
-  id: crypto.randomUUID(),
-  title: getRandomArrayElement(OFFERS),
-  price: getRandomInt()
-});
